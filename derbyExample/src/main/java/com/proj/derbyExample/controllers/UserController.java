172537ea4b43f09408e0feb0924cb05e3c259188
@nsengiyumv33 <nsengiyumv33@gmail.com>
2022-06-23 14:50:59 +0200

    bunch of sb structure files

---
+++ derbyExample/src/main/java/com/proj/derbyExample/controllers/UserController.java
@@ -0,0 +1,9 @@
+package com.proj.derbyExample.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class UserController {
+
+}
---
+++ derbyExample/src/main/java/com/proj/derbyExample/respository.java
@@ -0,0 +1,4 @@
+package com.proj.derbyExample;
+
+public class respository {
+}
