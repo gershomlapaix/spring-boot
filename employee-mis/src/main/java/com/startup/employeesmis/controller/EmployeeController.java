af74af7f356074c42ccbca41522331d7adbeccb6
@nsengiyumv33 <nsengiyumv33@gmail.com>
2022-06-23 18:57:59 +0200

    employee controller

--- employee-mis/src/main/java/com/startup/employeesmis/controller/EmployeeController.java
+++ employee-mis/src/main/java/com/startup/employeesmis/controller/EmployeeController.java
@@ -1,4 +1,25 @@
 package com.startup.employeesmis.controller;
 
+import com.startup.employeesmis.models.Employee;
+import com.startup.employeesmis.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@RestController
 public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping("/showEmployees")
+    public ModelAndView showEmployees(){
+        ModelAndView mav = new ModelAndView("list-employees");
+        List<Employee> list = employeeRepository.findAll();
+        mav.addObject("employees", list);
+        return mav;
+    }
 }
