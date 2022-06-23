9d821981102320e35738aa0d6d3d50e3ca9d5356
@nsengiyumv33 <nsengiyumv33@gmail.com>
2022-06-23 15:11:48 +0200

    user service

--- derbyExample/src/main/java/com/proj/derbyExample/services/UserService.java
+++ derbyExample/src/main/java/com/proj/derbyExample/services/UserService.java
@@ -1,17 +1,24 @@
 package com.proj.derbyExample.services;
 
 import com.proj.derbyExample.models.User;
+import com.proj.derbyExample.respository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class UserService {
-
+    @Autowired
+    private UserRepository userRepository;
     public void addUser(User user) {
+        userRepository.save(user);
     }
 
     public List<User> getAllUsers() {
-
+        List<User> users = new ArrayList<>();
+        userRepository.findAll().forEach(users::add);
+        return users;
     }
 }
